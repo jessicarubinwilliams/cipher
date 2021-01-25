bcf1cecebcf337b508ffaa569629b999db33b494
Jessica R. Williams <jessicarubinwilliams@gmail.com>
2021-01-25 08:27:50 -0800

    Change output function to return user's input concatenated with a ciphered code.

--- js/scripts.js
+++ js/scripts.js
@@ -11,7 +11,7 @@ $(document).ready(function() {
   };
   
   function output(letters) {
-    return lastFirst(letters);
+    return userSentence + lastFirst(letters) +".";
   }
 
   console.log(output(firstLast));
